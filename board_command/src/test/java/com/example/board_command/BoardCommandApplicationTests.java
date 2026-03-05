c8cdc0e5eeb266667b90f3f38d774656da328631
saralove20 <saralove20@naver.com>
2026-03-05 21:41:31 +0900

    [Init] 멀티 모듈 프로젝트 초기 세팅

---
+++ board_command/src/test/java/com/example/board_command/BoardCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board_command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardCommandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
